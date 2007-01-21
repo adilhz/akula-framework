46c8c07d879188d591d0e5606e01d00c8b9f7f12
krasimir.vanev <krasimir.vanev@c8178fe2-fe25-0410-a5b8-71190234a90f>
2007-01-21 12:36:03 +0000

    Fit http server to the new reactor api

--- branches/lf_reactor_integration/src/lf_http_server/server.h
+++ branches/lf_reactor_integration/src/lf_http_server/server.h
@@ -1,70 +1,46 @@
 /**
-	@file server.h 
-	
-	@brief Interface for CTCPServerSocketLogic, CServer classes
- */
+    @file server.h 
+*/
 
 #ifndef SERVER_H
 #define SERVER_H
 
-#include "event_handlers.h"
-#include "lf_thread_pool.h"
-#include "connections_table.h"
 #include <akula/reactor/reactor.h>
 #include <string>
-#include <functional>                   /*std::unary_function<>*/
 
 //forward decl.
 namespace net 
 {
     class CTCPServerSocket;
+    class CSocket;
     class CAddress;
 }
-class CLFThreadPool;
-class CTCPServerSocketLogic;
 
 /**
  * @class CServer
  * @brief The server
  */
-class CServer
+class CServer : reactor::CReactorUtils::IEventHandler
 {
 private:
-    reactor::Reactor_t* m_pReactor;
-    CLFThreadPool* m_pThreadPool;
-    ConnectionsTable_t* m_pConnectionsTable;
-    CReceiver* m_pReceiver;
-    CSender* m_pSender;
+    reactor::CReactor* m_pReactor;
     net::CAddress* m_pServerAddress;
     net::CTCPServerSocket* m_pTCPServerSocket;
-    CTCPServerSocketLogic* m_pTCPServerSocketLogic;
-    CSocketCallbackDecorator* m_pServerSocketDecorator;
-    bool m_bInitialized;
 
-    /** Server thread's logic - join to the server's thread pool*/
-    struct SServerThread : public std::unary_function<void*,void*>
-    {
-        void* operator()(void* parg)
-        {
-            CLFThreadPool* pThreadPool = reinterpret_cast<CLFThreadPool*>(parg);
-            if(pThreadPool)
-            {
-                pThreadPool->join();
-            }
-            
-            return NULL;
-        }
-    };
-
-    /**Initialize the server
+    /**
+    *   Initialize the server
     */
     bool initialize(const std::string&);
+
+    bool handle_read(net::CSocket* pSocket);
 public:
     CServer();
 
     ~CServer();
 
-    /**Start the server*/
+    /**
+    *   Start the server
+    */
     bool start(const std::string&, const int);
 };
 
